e541de37e7010dc597d1578c2cd058808d89b75b
ds <1159629515@qq.com>
2025-04-20 23:51:29 +0800

    ds

--- aether-core/src/main/java/com/ds/aether/core/model/client/RegisterParam.java
+++ aether-core/src/main/java/com/ds/aether/core/model/client/RegisterParam.java
@@ -14,10 +14,24 @@ public class RegisterParam implements Serializable {
 
     private static final long serialVersionUID = 1350156985574491056L;
 
+    /**
+     * 执行器所属名称
+     */
+    private String appName;
+
+    /**
+     * 执行器名称
+     */
     private String name;
 
+    /**
+     * 执行器地址
+     */
     private String host;
 
+    /**
+     * 请求执行器上下文路径
+     */
     private String contextPath;
 
 }
