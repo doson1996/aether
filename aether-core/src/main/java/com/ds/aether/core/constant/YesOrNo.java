ca29b57b50147e6421f653a95ff619669abc4e9e
ds <1159629515@qq.com>
2025-08-13 17:42:29 +0800

    分布式调度

---
+++ aether-core/src/main/java/com/ds/aether/core/constant/YesOrNo.java
@@ -0,0 +1,14 @@
+package com.ds.aether.core.constant;
+
+/**
+ * @author ds
+ * @date 2025/8/13
+ * @description
+ */
+public class YesOrNo {
+
+    public static final Integer YES = 1;
+
+    public static final Integer NO = 0;
+
+}
