b8e2b74713405dc9f386ff2ab8e6a67f7047e717
ds <1159629515@qq.com>
2025-08-14 17:58:54 +0800

    执行任务上送执行器名

---
+++ aether-client/src/main/java/com/ds/aether/client/job/JavaJobInfo.java
@@ -0,0 +1,17 @@
+package com.ds.aether.client.job;
+
+import com.ds.aether.core.model.ExecJobParam;
+
+/**
+ * @author ds
+ * @date 2025/8/14
+ * @description
+ */
+public class JavaJobInfo implements JobInfo {
+
+    @Override
+    public void execute(ExecJobParam execJobParam) {
+
+    }
+
+}
